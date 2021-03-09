ccc631ec3740a6a7ca3aca666ed5acfc75539f3d
Olivier <oliviermcgee@gmail.com>
2021-03-09 15:18:03 -0500

    Jquery and JS
    Added jquery and menu selection

---
+++ script.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+    $("#layer").change(function() {
+        var val = $(this).val();
+        if (val == "0") {
+            $("#region").html("<option value='0.0'>Select a region</option>");
+        } else if (val == "1") {
+            $("#region").html("<option value='0.0'>Select a region</option> <option value='1.1'>First Region</option><option value='1.2'>Second Region</option>");
+        } else if (val == "2") {
+            $("#region").html("<option value='0.0'>Select a region</option> <option value='2.1'>First Region</option><option value='2.2'>Second Region</option>");
+
+        } else if (val == "3") {
+            $("#region").html("<option value='0.0'>Select a region</option> <option value='3.1'>First Region</option><option value='3.2'>Second Region</option>");
+
+        } else if (val == "4") {
+            $("#region").html("<option value='0.0'>Select a region</option> <option value='4.1'>First Region</option><option value='4.2'>Second Region</option>");
+
+        }
+    });
+
+    $("#region").change(function() {
+        var val = $(this).val();
+        if (val == "0.0") {
+            $("#monster").html("<option value='0.0.0'>Select a monster</option>");
+        } else if (val == "1.1") {
+            $("#monster").html("<option value='0.0.0'>Select a monster</option> <option value='1.1.1'>Tentacle Goat</option><option value='1.1.2'>Swarm of Insects</option> <option value='1.1.3'>hippogriff</option> <option value='1.1.4'>Five-Eyed Vulture</option> <option value='1.1.3'>Basilisk</option>");
+        } else if (val == "1.2") {
+            $("#monster").html("<option value='0.0.0'>Select a monster</option> <option value='1.2.2'>Fanged Harvestman</option> <option value='1.2.2'>Owlbear</option>");
+
+        } else if (val == "2.1") {
+            $("#monster").html("<option value='0.0.0'>Select a monster</option> <option value='2.1.1'>Manticore</option><option value='2.1.2'>Phase Spider</option> <option value='2.1.3'>Corpse Weeper</option> <option value='2.1.4'>Melting Death</option> <option value='1.2.2'>Owlbear</option>");
+
+        } else if (val == "2.2") {
+            $("#monster").html("<option value='0.0.0'>Select a monster</option> <option value='2.2.1'>Armored Ape</option><option value='2.2.2'>Dismemberment Troll</option> <option value='2.2.3'>Primordial Ape</option>");
+
+        }else if (val == "3.1") {
+            $("#monster").html("<option value='0.0.0'>Select a monster</option> <option value='3.1.1'>Chuul</option><option value='3.1.2'>Aboleth</option> <option value='3.1.3'>Ripper</option> <option value='3.1.4'>The Maw</option>");
+
+        }else if (val == "3.2") {
+            $("#monster").html("<option value='0.0.0'>Select a monster</option> <option value='3.2.1'>No Entry</option>");
+
+        }else if (val == "4.1") {
+            $("#monster").html("<option value='0.0.0'>Select a monster</option> <option value='4.1.1'>Old Stinkeye</option><option value='4.1.2'>Water Leaper</option> <option value='4.1.3'>Hydra</option>");
+
+        }
+    });
+});
